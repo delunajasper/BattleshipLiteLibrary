b27aff674cc404c0e7dab8e8503d0ce5c6de6182
Jasper DeLuna <jasper.deluna@aol.com>
2020-08-31 23:35:13 +0100

    Add project files.

---
+++ GameLogic.cs
@@ -0,0 +1,59 @@
+﻿using BattleshipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary
+{
+    //static no need to store data, logic methods only for reusability
+    public static class GameLogic
+    {
+        //Objects instantiated / class instances, dont pass copy just the address
+        //passing in model
+        //initialise grid using parameters
+        public static void InitializeGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1, 2, 3, 4, 5
+            };
+
+            foreach (string letter in letters)
+            {
+                foreach(int number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+            }
+        }
+
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            //gona take model and add gridspot
+            GridSpotModel spot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+               Status = GridSpotStatus.Empty
+            };
+
+            model.ShotGrid.Add(spot);
+        }
+
+        public static bool PlaceShip(PlayerInfoModel model, string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
